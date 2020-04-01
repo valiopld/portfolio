187a849b58beb75256f54b902d9948b09766c584
Valentin <valiopld@gmail.com>
2020-04-01 20:27:30 +0300

    Updates

---
+++ precache-manifest.bea5fa8de49b139598de780c5b76ca4a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3057c855bae99bd6aadd81734330a467",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1cf4a10f5c3f005786ef",
+    "url": "/portfolio/static/css/main.974654a1.chunk.css"
+  },
+  {
+    "revision": "f77b7abb56562c613a62",
+    "url": "/portfolio/static/js/2.099babd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.099babd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf4a10f5c3f005786ef",
+    "url": "/portfolio/static/js/main.1a3f9174.chunk.js"
+  },
+  {
+    "revision": "5d02faeed550940eceb4",
+    "url": "/portfolio/static/js/runtime-main.999d3220.js"
+  },
+  {
+    "revision": "faef7d01b7864621232149e4bd29339b",
+    "url": "/portfolio/static/media/PlovdivDisplay-Bold.faef7d01.otf"
+  },
+  {
+    "revision": "287d0ddddfe80d42220494aa355e72bb",
+    "url": "/portfolio/static/media/PlovdivSans.287d0ddd.otf"
+  },
+  {
+    "revision": "8f1fc0ba0bc06b254b3c8fa61e4b8292",
+    "url": "/portfolio/static/media/facebook.8f1fc0ba.svg"
+  },
+  {
+    "revision": "79e19b80d9113efe384bcbe3e6c205c7",
+    "url": "/portfolio/static/media/game.79e19b80.png"
+  },
+  {
+    "revision": "67910a24ef067bf5dc2400ab841889c4",
+    "url": "/portfolio/static/media/github.67910a24.svg"
+  },
+  {
+    "revision": "75e4ee7f06f11d8b95928909608db154",
+    "url": "/portfolio/static/media/linkedin.75e4ee7f.svg"
+  },
+  {
+    "revision": "f9fe575b5769f2ce508c487b90ba6cc4",
+    "url": "/portfolio/static/media/logo.f9fe575b.png"
+  },
+  {
+    "revision": "621fb1cc1bf6d37f2d2af1ad17fdec3e",
+    "url": "/portfolio/static/media/mail.621fb1cc.svg"
+  },
+  {
+    "revision": "6947aacdacc07a5cbe395399b9feb224",
+    "url": "/portfolio/static/media/profile.6947aacd.jpg"
+  },
+  {
+    "revision": "ff3ff70299b5004d877a924d12c8ae5d",
+    "url": "/portfolio/static/media/txtEff.ff3ff702.png"
+  },
+  {
+    "revision": "c53248849ab5dbc57a97fcfe3bf9f872",
+    "url": "/portfolio/static/media/web.c5324884.png"
+  }
+]);
