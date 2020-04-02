9c3b4ef3ea49844a2ad2d9d0c9f99192f1dbaaa7
Valentin <valiopld@gmail.com>
2020-04-02 11:49:28 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f18161b5809ed11126ec8811525c4aed.js"
+  "/portfolio/precache-manifest.bea5fa8de49b139598de780c5b76ca4a.js"
 );
 
 self.addEventListener('message', (event) => {
